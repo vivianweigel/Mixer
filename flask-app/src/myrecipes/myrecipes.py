24a2045ba63d8af0cea9909ba8983e1429fd5512
Julia Knopp <juliaknopp@Julias-MacBook-Air.local>
2023-04-12 19:06:21 -0400

    add

--- flask-app/src/myrecipes/myrecipes.py
+++ flask-app/src/myrecipes/myrecipes.py
@@ -31,6 +31,32 @@ def get_recipes():
 
     return jsonify(json_data)
 
+@myrecipes.route('/mine', methods=['GET'])
+def get_mine():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT recipe_name FROM Personal_recipes WHERE user_id = 1')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
 # # get the top 5 products from the database
 # @products.route('/mostExpensive')
 # def get_most_pop_products():
